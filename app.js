3b3378725c38d45a701b5ff8d5d81a0079d69538
‘Cait <caitleigh27@gmail.com>
2019-11-27 20:14:45 -0800

    added propert that tracks clicks and views

--- app.js
+++ app.js
@@ -4,17 +4,16 @@
 var picOne = document.getElementById('picture1');
 var picTwo = document.getElementById('picture2');
 var picThree = document.getElementById('picture3');
+var productOptions = document.getElementById('product-options');
 var picArray = [];
-
-//display images
-
-
 
 //constructor funciton for products
 function Product (src, name) {
   this.src = `../img/${src}.jpg`;
   this.title = name;
   this.alt = name;
+  this.clicked = 0;
+  this.viewed = 0;
 
   picArray.push(this);
 
@@ -29,24 +28,45 @@ function generateImages() {
   var index = randomIndex(picArray.length);
   picOne.src = picArray[index].src;
   picOne.title = picArray[index].title;
-  picOne.alt = picArray[index].title;
+  picOne.alt = picArray[index].alt;
+  picArray[index].viewed ++;
+
+
   var index2 = randomIndex(picArray.length);
   picTwo.src = picArray[index2].src;
-  picTwo.title = picArray[index].title;
-  picTwo.alt = picArray[index].title;
+  picTwo.title = picArray[index2].title;
+  picTwo.alt = picArray[index2].alt;
+  picArray[index2].viewed ++;
+
   while (index2 === index) {
     index2 = randomIndex(picArray.length);
   }
+
   var index3 = randomIndex(picArray.length);
   picThree.src = picArray[index3].src;
-  picThree.title = picArray[index].title;
-  picThree.alt = picArray[index].title;
+  picThree.title = picArray[index3].title;
+  picThree.alt = picArray[index3].alt;
+  picArray[index3].viewed ++;
 
-  while (index3 === index || index3 === index2) {
+
+  while (index3 === index && index3 === index2) {
     index3 = randomIndex(picArray.length);
   }
   console.log (index, index2, index3);
+}
 
+//add an event listener
+
+function handleClick(event) {
+  var vote = event.target.title;
+  console.log (vote, 'was clicked');
+  for (var i = 0; i < picArray.length; i++) {
+    if(vote === picArray[i].title) {
+      picArray[i].clicked++;
+    }
+  }
+  generateImages();
+  console.table(picArray);
 }
 
 function createOnPageLoad() {
@@ -73,12 +93,9 @@ function createOnPageLoad() {
 }
 
 
-
 createOnPageLoad();
-
-console.table(picArray);
-
-console.log (generateImages());
+productOptions.addEventListener('click', handleClick);
 
 generateImages();
 
+console.table(picArray);
